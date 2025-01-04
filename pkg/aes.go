18db700239f0f3a5383654efa250e22e6450b8f5
ffrancon <ffranconwd@gmail.com>
2025-01-04 18:04:01 +0100

    add AES CBC encrypt func

--- pkg/aes.go
+++ pkg/aes.go
@@ -14,3 +14,22 @@ func AesECBDecrypt(ciphertext, key []byte) []byte {
 	}
 	return decrypted
 }
+
+func AESCBCEncrypt(plaintext, key, iv []byte) []byte {
+	cipher, err := aes.NewCipher(key)
+	Check(err)
+	keySize := len(key)
+	paddedTxt := AddPKCS7Padding(plaintext, keySize)
+	chunks := ChunkBytes(paddedTxt, keySize)
+	encrypted := make([]byte, len(paddedTxt))
+
+	for i, c := range chunks {
+		if i == 0 {
+			cipher.Encrypt(encrypted[:keySize], XorBytes(c, iv))
+		} else {
+			cipher.Encrypt(encrypted[i*keySize:], XorBytes(c, encrypted[(i-1)*keySize:i*keySize]))
+		}
+	}
+
+	return encrypted
+}
