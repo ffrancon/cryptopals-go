fca3d382883b68a286f7d2d8af77334179b98f7e
Florian Françon <florian@batch.com>
2025-11-10 10:49:50 +0100

    refactoring chal12 oracle w/ struct

--- internal/oracle/oracle_12.go
+++ internal/oracle/oracle_12.go
@@ -10,20 +10,27 @@ import (
 	"os"
 )
 
-var key = utils.GenerateRandomBytes(16)
 var b64SecretString = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
 
-func encryptWithSecretString(bytes, secret, key []byte) []byte {
-	combined := append(bytes, secret...)
-	return aes.AESECBEncrypt(combined, key)
-
+type SecretOracle struct {
+	key []byte
 }
 
-func findAESKeySize() int {
+func NewSecretOracle() *SecretOracle {
+	return &SecretOracle{
+		key: utils.GenerateRandomBytes(16),
+	}
+}
+
+func (o *SecretOracle) encryptWithSecretString(bytes, secret []byte) []byte {
+	combined := append(bytes, secret...)
+	return aes.AESECBEncrypt(combined, o.key)
+}
+
+func (o *SecretOracle) findAESKeySize() int {
 	prevLen := 0
 	for i := range 32 {
-		output := aes.AESECBEncrypt(bytes.Repeat([]byte("A"), i+1), key)
-		fmt.Printf("Output length at iteration %d: %d\n", i, len(output))
+		output := aes.AESECBEncrypt(bytes.Repeat([]byte("A"), i+1), o.key)
 		// If the output length is different from the previous output length, we found the key size
 		if len(output) != prevLen && prevLen != 0 {
 			return i
@@ -34,15 +41,16 @@ func findAESKeySize() int {
 }
 
 func AESECBOracle() (decrypted []byte) {
-	// Use a sample of 32 bytes to find the key size
-	keysize := findAESKeySize()
+	oracle := NewSecretOracle()
+
+	keysize := oracle.findAESKeySize()
 	if keysize == 0 {
 		fmt.Println("Could not determine key size")
 		return
 	}
 
 	// Check if the encryption mode is ECB
-	isECBMode := scoring.ScoringECBMode(aes.AESECBEncrypt(bytes.Repeat([]byte("A"), keysize*2), key), keysize) > 0
+	isECBMode := scoring.ScoringECBMode(aes.AESECBEncrypt(bytes.Repeat([]byte("A"), keysize*2), oracle.key), keysize) > 0
 	if !isECBMode {
 		fmt.Println("The encryption mode is CBC")
 		return
@@ -68,13 +76,13 @@ func AESECBOracle() (decrypted []byte) {
 			}
 			// This block is i byte(s) short of the keysize
 			block := make([]byte, keysize-1-k)
-			encryptedBlock := encryptWithSecretString(block, c, key)[:keysize]
+			encryptedBlock := oracle.encryptWithSecretString(block, c)[:keysize]
 			// We will test all the possible values for the last byte of the block until we find the one that matches the encrypted block
 			for j := range 256 {
 				// Reconstruct the full block
 				reconstructed := append(block, chunk[:k]...)
 				reconstructed = append(reconstructed, byte(j))
-				encryptedReconstructed := aes.AESECBEncrypt(reconstructed, key)
+				encryptedReconstructed := aes.AESECBEncrypt(reconstructed, oracle.key)
 				if bytes.Equal(encryptedBlock, encryptedReconstructed[:keysize]) {
 					chunk[k] = byte(j)
 					break
